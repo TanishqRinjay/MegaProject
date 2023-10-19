674c17f1b9769c3a1176d5f6797bbbd70b6774b7
Tanishq Rinjay <tanishqbaranwal@gmail.com>
2023-10-19 21:31:08 +0530

    changed token and cookie expiry to 30 days

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -193,14 +193,14 @@ exports.login = async (req, res) => {
                 accountType: user.accountType,
             };
             const token = jwt.sign(payload, process.env.JWT_SECRET, {
-                expiresIn: "2h",
+                expiresIn: "30d",
             });
             user.token = token;
             user.password = undefined;
 
             //Create cookie and send response
             const options = {
-                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
                 httpOnly: true,
             };
             res.cookie("token", token, options).status(200).json({
